a6b24e3db12795e4af331a71537713296ef6f35f
alan.r <reale.ag@gmail.com>
2022-08-22 21:45:39 -0300

    tirar cambios

---
+++ Desktop/Proyectos/Coderhouse/React JS/reactjs/src/App.js
@@ -0,0 +1,14 @@
+import './estilos.css';
+import 'core-js/actual';
+
+function App() {
+  console.log(Array.from(new Set([1, 2, 3, 4, 3, 2, 1, 1])));
+
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
